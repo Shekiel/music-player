e1bb163b4eef5ae6dac05f09688eedd07cdc2330
shekiel <shekielmorris1@gmail.com>
2021-09-25 23:37:57 +0100

    delete test tracks

--- script.js
+++ script.js
@@ -13,26 +13,6 @@ const nextBtn = document.getElementById('next');
 // Music
 const songs = [
   {
-    name: 'jacinto-1',
-    displayName: 'Electric Chill Machine',
-    artist: 'Jacinto Design',
-  },
-  {
-    name: 'jacinto-2',
-    displayName: 'Seven Nation Army (Remix)',
-    artist: 'Jacinto Design',
-  },
-  {
-    name: 'jacinto-3',
-    displayName: 'Goodnight, Disco Queen',
-    artist: 'Jacinto Design',
-  },
-  {
-    name: 'metric-1',
-    displayName: 'Front Row (Remix)',
-    artist: 'Metric/Jacinto Design',
-  },
-  {
     name: 'three-little-birds',
     displayName: '3 Little Birds',
     artist: 'Bob Marley',
